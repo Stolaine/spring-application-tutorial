862eafbd5b06c5405d8e0830c3b1682e0038efcc
stolaine <sheshank238mukul@gmail.com>
2022-09-14 01:38:57 +0530

    Convert VandeMatram POJO to Bean

--- src/main/java/com/stolaine/spring/model/VandeMatram.java
+++ src/main/java/com/stolaine/spring/model/VandeMatram.java
@@ -1,5 +1,8 @@
 package com.stolaine.spring.model;
 
+import org.springframework.stereotype.Component;
+
+@Component("vandeMatram")
 public class VandeMatram implements CompactDisc {
     private String title = "Vande Matram";
     private String artist = "A. R. Rehmaan, Late Lata Mangeshkar";
