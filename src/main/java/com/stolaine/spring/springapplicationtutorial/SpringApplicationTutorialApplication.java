2c1655f082c0979b320bf140594cbee6eab86540
stolaine <sheshank238mukul@gmail.com>
2022-09-14 01:23:42 +0530

    Initial commit

---
+++ src/main/java/com/stolaine/spring/springapplicationtutorial/SpringApplicationTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.stolaine.spring.springapplicationtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplicationTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stolaine/spring/springapplicationtutorial/SpringApplicationTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stolaine.spring.springapplicationtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
